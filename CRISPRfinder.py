143351208bdbc501150b520646b2c4371e0dea8e
Jon Lee <43578738+JoneLee316@users.noreply.github.com>
2020-06-04 13:30:57 -0400

    Update CRISPRfinder.py
    changed output file and aded chr#

--- CRISPRfinder.py
+++ CRISPRfinder.py
@@ -7,7 +7,11 @@ import re
 PAMsite = "NGG"
 REVcomp = "CCN"
 infile = "Homo_sapiens.GRCh38.dna.chromosome.1.fa"
+
+#have the outfile and chr# auto fill 
+#do this in code
 outfile = "chr1.CRISPRsites.txt"
+chromosome = "1"
 
 ##### NOTES #####
 #When searching for new PAM sites
@@ -17,25 +21,25 @@ outfile = "chr1.CRISPRsites.txt"
 ##### CODE #####
 #generate reverse compliment from PAMsite
 #add capability for regex to automatically search when changing PAMsite
-print("Location" + "\t" + "Fasta_file" + "\t" + "Gen_Pattern" + "\t" + "Specific_Site")
+print("Chr#" + "\t" + "Position" + "\t" + "Gen_Pattern" + "\t" + "Specific_Site" + "\t" + "Fasta_file" + "\n")
 
 with open(infile) as f:
     sequence = [line.rstrip() for line in f]
     sequence = "".join(sequence[1:])
 
 output = open(outfile, 'w')
-output.write("Location" + "\t" + "Fasta_file" + "\t" + "Gen_Pattern" + "\t" + "Specific_Site" + "\n")
+output.write("Chr#" + "\t" + "Position" + "\t" + "Gen_Pattern" + "\t" + "Specific_Site" + "\t" + "Fasta_file" + "\n")
 
-for i in range(0, len(sequence)-3):
+for i in range(0, 10500): #len(sequence)-3
     trimer = sequence[i] + sequence[i+1] + sequence[i+2]
-    location = str(i)
+    position = str(i)
 
     if re.search(r'\wGG', trimer):
-        print(location + "\t" + infile + "\t" + PAMsite + "\t" + trimer)
-        output.write(location + "\t" + infile + "\t" + PAMsite + "\t" + trimer + "\n")
+        print(chromosome + "\t" + position + "\t" + PAMsite + "\t" + trimer + "\t" + infile)
+        output.write(chromosome + "\t" + position + "\t" + PAMsite + "\t" + trimer + "\t" + infile + "\n")
     
     if re.search(r'CC\w', trimer):
-        print(location + "\t" + infile + "\t" + REVcomp + "\t" + trimer)
-        output.write(location + "\t" + infile + "\t" + REVcomp + "\t" + trimer + "\n")
+        print(chromosome + "\t" + position + "\t" + REVcomp + "\t" + trimer + "\t" + infile)
+        output.write(chromosome + "\t" + position + "\t" + REVcomp + "\t" + trimer + "\t" + infile + "\n")
 
 output.close()
