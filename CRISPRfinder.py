6c1f09b2461ee94b9e25f16fa21cecf2110ca772
Jon Lee <43578738+JoneLee316@users.noreply.github.com>
2020-06-24 10:32:10 -0400

    added dictionary for symbols

--- CRISPRfinder.py
+++ CRISPRfinder.py
@@ -3,7 +3,12 @@ import re
 import sys
 
 infile = sys.argv[1]
-PAMsite = sys.argv[2] # change to read in file of list of PAM sites
+PAMsite = sys.argv[2]
+
+# make sure all input files are names *.fa (* being chromosome no.)
+chromosome = infile.split(".")[0]
+
+outfile = chromosome + ".CRISPRsites.txt"
 
 PAMrev = PAMsite[::-1]
 
@@ -11,85 +16,16 @@ PAMsiteRE = ''
 REVcompRE = ''
 REVcomp = ""
 
-# make sure all input files are names *.fa (* being chromosome no.)
-chromosome = infile.split(".")[0]
+PAMdictRE = {"N":r'\w', "A": 'A', "T":'T', "C":'C', "G":'G', "Y":'(T|C)', "R":'(A|G)', "W":'(A|T)', "V":'(A|C)', "S":'(G|C)', "K":'(G|T)', "H":'(A|T|C)', "B":'(G|C|T)', "D":'(G|A|T)'}
+REVdictRE = {"N":r'\w', "A": 'T', "T":'A', "C":'G', "G":'C', "Y":'(A|G)', "R":'(T|C)', "W":'(T|A)', "V":'(T|G)', "S":'(C|G)', "K":'(C|A)', "H":'(T|A|G)', "B":'(C|G|A)', "D":'(C|T|A)'}
+REVdictSYM = {"N":'N', "A": 'T', "T":'A', "C":'G', "G":'C', "Y":'R', "R":'Y', "W":'W', "V":'K', "S":'S', "K":'V', "H":'D', "B":'b', "D":'H'}
 
-outfile = chromosome + ".CRISPRsites.txt"
-
-# make dictionary of each shorthand letter to make it easier to read - possibly
 for i in PAMsite:
-    if i == "N":
-        PAMsiteRE = PAMsiteRE + r'\w'
-    if i == "A":
-        PAMsiteRE = PAMsiteRE + 'A'
-    if i == "T":
-        PAMsiteRE = PAMsiteRE + 'T'
-    if i == "C":
-        PAMsiteRE = PAMsiteRE + 'C'
-    if i == "G":
-        PAMsiteRE = PAMsiteRE + 'G'
-    if i == "Y":
-        PAMsiteRE = PAMsiteRE + '(T|C)'
-    if i == "R":
-        PAMsiteRE = PAMsiteRE + '(A|G)'
-    if i == "W":
-        PAMsiteRE = PAMsiteRE + '(A|T)'
-    if i == "V":
-        PAMsiteRE = PAMsiteRE + '(A|C)'
-    if i == "S":
-        PAMsiteRE = PAMsiteRE + '(G|C)'
-    if i == "K":
-        PAMsiteRE = PAMsiteRE + '(G|T)'
-    if i == "H":
-        PAMsiteRE = PAMsiteRE + '(A|T|C)'
-    if i == "B":
-        PAMsiteRE = PAMsiteRE + '(G|C|T)'
-    if i == "D":
-        PAMsiteRE = PAMsiteRE + '(G|A|T)'
+    PAMsiteRE = PAMsiteRE + PAMdictRE[i]
 
 for i in PAMrev: 
-    if i == "N":
-        REVcomp = REVcomp + i # reverse compliment stays the same
-        REVcompRE = REVcompRE + r'\w'
-    if i == "A":
-        REVcomp = REVcomp + "T"
-        REVcompRE = REVcompRE + 'T'
-    if i == "T":
-        REVcomp = REVcomp + "A"
-        REVcompRE = REVcompRE + 'A'
-    if i == "C":
-        REVcomp = REVcomp + "G"
-        REVcompRE = REVcompRE + 'G'
-    if i == "G":
-        REVcomp = REVcomp + "C"
-        REVcompRE = REVcompRE + 'C'
-    if i == "Y":
-        REVcomp = REVcomp + "R" # R and Y swap for reverse compliment
-        REVcompRE = REVcompRE + '(A|G)'
-    if i == "R":
-        REVcomp = REVcomp + "Y"
-        REVcompRE = REVcompRE + '(T|C)'
-    if i == "W":
-        REVcomp = REVcomp + "W" # reverse compliment stays the same
-        REVcompRE = REVcompRE + '(A|T)' # T|A
-    if i == "V":
-        REVcomp = REVcomp + "K" # V and K swap for reverse compliment
-        REVcompRE = REVcompRE + '(T|G)' # T|G
-    if i == "S":
-        REVcomp = REVcomp + "S" # reverse compliment stays the same
-        REVcompRE = REVcompRE + '(G|C)' # G|C
-    if i == "K":
-        REVcomp = REVcomp + "V" 
-        REVcompRE = REVcompRE + '(C|A)' # C|A
-    if i == "H":
-        REVcomp = REVcomp + "D" # H and D swap for reverse compliment
-        REVcompRE = REVcompRE + '(G|A|T)' # T|A|G
-    if i == "B":
-        REVcomp = REVcomp + "b" # no reverse compliment exists in the current line up
-        REVcompRE = REVcompRE + '(G|C|T)' # C|G|A
-    if i == "D":
-        REVcomp = REVcomp + "G"
-        REVcompRE = REVcompRE + '(A|T|C)' # C|T|A
+    REVcomp = REVcomp + REVdictSYM[i]
+    REVcompRE = REVcompRE + REVdictRE[i]
 
 # print("Chr#" + "\t" + "Position" + "\t" + "Gen_Pattern" + "\t" + "Specific_Site" + "\t" + "Fasta_file" + "\n")
 
@@ -107,8 +43,6 @@ for i in range(0, len(sequence)-3):
 
     position = str(i)
 
-    #add for loop to loop through array of PAMsite/REVcomp
-    #have search be automatic based on PAMsite and REVcomp
     if re.search(PAMsiteRE, searchSEQ):
         # print(chromosome + "\t" + position + "\t" + PAMsite + "\t" + searchSEQ + "\t" + infile)
         output.write(chromosome + "\t" + position + "\t" + PAMsite + "\t" + searchSEQ + "\t" + infile + "\n")
